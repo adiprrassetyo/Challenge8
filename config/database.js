456d18042cefba4d3f85ad4d87c83d5e993461f6
adeeep14 <disconetct@gmail.com>
2022-11-17 00:47:03 +0700

    test1

--- config/database.js
+++ config/database.js
@@ -22,7 +22,7 @@ module.exports = {
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres"
